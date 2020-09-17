03da8d2005f826919c215b53542b4829558750b2
thangqt <63841132+thangqt@users.noreply.github.com>
2020-09-17 09:56:33 +0700

    asdasda
    sadasdsada

--- src/main/java/app/thang/ThangApplication.java
+++ src/main/java/app/thang/ThangApplication.java
@@ -2,9 +2,16 @@ package app.thang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ThangApplication {
+	
+		@RequestMapping("/hello")
+	public String helloDocker() {
+		return "Hello Docker!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThangApplication.class, args);
