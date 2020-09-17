f5563c177153dc7a127572ccd2be8d4a45854bd4
thangqt <thang.quoctruong1204@gmail.com>
2020-09-17 02:52:54 +0000

    add file

---
+++ src/main/java/app/thang/ThangApplication.java
@@ -0,0 +1,13 @@
+package app.thang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/thang/ThangApplicationTests.java
@@ -0,0 +1,13 @@
+package app.thang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
